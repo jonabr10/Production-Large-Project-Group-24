bccbea131307a98d9b3ba6ed1bd2361914a1ed0c
Daniel Sanchez <sancd22@gmail.com>
2021-07-18 17:32:57 -0400

    Initial login and register. Starting API connect

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,39 +2,47 @@ import React, { Component } from "react";
 import './css/LoginAndSignup.css'
 
 export default class Login extends Component {
-    render() {
-        return (
-            <form>
+    constructor(props)
+    {
+        super(props);
+        this.state = 
+        {
+            username: '',
+            password: ''
+        }
+    }
 
+    myFunc = () =>
+    {
+        alert(this.state.username + ' ' + this.state.password);
+    }
+
+    handleInputChange = ({ target }) => 
+    {
+        this.setState({ [target.name]: target.value });
+    }
+
+    render() {
+        return (    
+            <form>
                 <h3>Log in</h3>
 
                 <div className="form-group">
-                    <label>Email</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <label>Username</label>
+                    <input type="text" name="username" className="form-control" placeholder="Enter username" onChange={this.handleInputChange} />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" name="password" className="form-control" placeholder="Enter password" onChange={this.handleInputChange} />
                 </div>
-
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
-
-                <button type="submit" className="btn btn-dark btn-lg btn-block" onClick={this.myFunc}>Sign in</button>
+                
+                <br></br>
+                <button type="submit" className="btn btn-dark btn-lg btn-block" onClick={() => this.myFunc()}>Sign in</button>
                 <p className="forgot-password text-right">
                     Forgot <a href="#">password?</a>
                 </p>
             </form>
         );
-    }
-
-    myFunc = () =>
-    {
-        alert('hi');
     }
 }
--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -2,6 +2,30 @@ import React, { Component } from "react";
 import './css/LoginAndSignup.css'
 
 export default class SignUp extends Component {
+    constructor(props)
+    {
+        super(props);
+        this.state = 
+        {
+            firstName: '',
+            lastName: '',
+            email: '',
+            username: '',
+            password: '',
+            confirmPassword: ''
+        }
+    }
+
+    myFunc = () =>
+    {
+        alert('user: ' + this.state.username + '\npass: ' + this.state.password + '\nfName: ' + this.state.firstName + '\nlName: ' + this.state.lastName + '\nemail: ' + this.state.email);
+    }
+
+    handleInputChange = ({ target }) => 
+    {
+        this.setState({ [target.name]: target.value });
+    }
+    
     render() {
         return (
             <form>
@@ -9,25 +33,36 @@ export default class SignUp extends Component {
 
                 <div className="form-group">
                     <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
+                    <input type="text" name="firstName" className="form-control" placeholder="First name" onChange={this.handleInputChange} />
                 </div>
 
                 <div className="form-group">
                     <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
+                    <input type="text" name="lastName" className="form-control" placeholder="Last name" onChange={this.handleInputChange} />
                 </div>
 
                 <div className="form-group">
                     <label>Email</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="text" name="email" className="form-control" placeholder="Enter email" onChange={this.handleInputChange} />
+                </div>
+
+                <div className="form-group">
+                    <label>Username</label>
+                    <input type="text" name="username" className="form-control" placeholder="Enter username" onChange={this.handleInputChange} />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" name="password" className="form-control" placeholder="Enter password" onChange={this.handleInputChange} />
                 </div>
 
-                <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
+                <div className="form-group">
+                    <label>Confirm Password</label>
+                    <input type="password" name="confirmPassword" className="form-control" placeholder="Confirm password" onChange={this.handleInputChange} />
+                </div>
+
+                <br></br>
+                <button type="submit" className="btn btn-dark btn-lg btn-block" onClick={() => this.myFunc()}>Register</button>
                 <p className="forgot-password text-right">
                     Already registered <a href="#">log in?</a>
                 </p>
