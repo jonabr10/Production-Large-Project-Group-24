c611808986dbd50a2830555261b7b162dd52916b
Jon Abrigo <jonabrigo@knights.ucf.edu>
2021-08-03 13:24:56 -0400

    Update sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -1,14 +1,14 @@
 const nodemailer = require("nodemailer");
-const { google } = require("googleapis");
+// const { google } = require("googleapis");
 let pathBuilder = require('./frontend/src/Path');
 
 require("dotenv").config();
 
 // googleapis credentials
-const CLIENT_ID = '817724187788-crtnre3l9af61fsouajbcfaf2kfsrc5s.apps.googleusercontent.com';
-const CLIENT_SECRET = '-3TDXi0telH7ylPozPP7GF_6';
-const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN = '1//04qJVMr85uwIHCgYIARAAGAQSNwF-L9IrytQfQ3P-qH3IXf-am01VXkMJGgSCCW21SpiaVdyrSmdMPLYYLIXi4UsdXxV-zS7NDnI';
+// const CLIENT_ID = '817724187788-crtnre3l9af61fsouajbcfaf2kfsrc5s.apps.googleusercontent.com';
+// const CLIENT_SECRET = '-3TDXi0telH7ylPozPP7GF_6';
+// const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+// const REFRESH_TOKEN = '1//04qJVMr85uwIHCgYIARAAGAQSNwF-L9IrytQfQ3P-qH3IXf-am01VXkMJGgSCCW21SpiaVdyrSmdMPLYYLIXi4UsdXxV-zS7NDnI';
 
 // OAuth2 Access
 // const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
@@ -17,18 +17,7 @@ const REFRESH_TOKEN = '1//04qJVMr85uwIHCgYIARAAGAQSNwF-L9IrytQfQ3P-qH3IXf-am01VX
 // // oAuth2 access token
 // const accessToken = oAuth2Client.getAccessToken();
 
-let transporter = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-        type: 'oauth2',
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD,
-        clientId: CLIENT_ID,
-        clientSecret: CLIENT_SECRET,
-        refreshToken: REFRESH_TOKEN,
-        accessToken: new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI).setCredentials({ refresh_token: REFRESH_TOKEN }),
-    },
-});
+
 
 // Incoming: email, uniqueString
 // Outgoing: account verification email
@@ -38,6 +27,19 @@ exports.sendVerification = async function (email, uniqueString) {
         // the api route contain the route to the api for verify and the uniqueString identifier for a
         // specific user
         var apiRoute = "verify/" + uniqueString;
+
+        let transporter = nodemailer.createTransport({
+            service: "yahoo",
+            auth: {
+                // type: 'oauth2',
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASSWORD,
+                // clientId: CLIENT_ID,
+                // clientSecret: CLIENT_SECRET,
+                // refreshToken: REFRESH_TOKEN,
+                // accessToken: new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI).setCredentials({ refresh_token: REFRESH_TOKEN }),
+            },
+        });
 
         // send mail with defined transport object
         let info = await transporter.sendMail({
